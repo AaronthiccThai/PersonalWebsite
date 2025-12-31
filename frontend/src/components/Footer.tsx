aa1442f81b4507d20aec25ce5508df5a617898e5
Aaron Thai <howaaronic1@gmail.com>
2025-12-31 15:44:38 +1100

    Added more information to about

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="bottom-0 left-0 w-full bg-[rgb(33,52,72)] text-white py-1">
       <div className="flex items-center justify-center px-30 py-4 text-white">
-        <span className="font-bold text-xl">© 2025 Aaron. All rights reserved.</span>
+        <span className="font-bold text-xl">© 2026 Aaron. All rights reserved.</span>
       </div>  
     </footer>
   )
--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,17 +1,51 @@
+import { Typewriter } from "react-simple-typewriter"
 const About = () => {
 
   return (
     <div className="relative min-h-screen overflow-hidden flex flex-col justify-center items-center"
       style={{["--bg-start" as any]: "#ead431", ["--bg-end" as any]: "#e9a398",}}
     >
-      <h1 className="z-10">Welcome to the About Page - WIP</h1>  
       <div className="area">
         <ul className="circles">
           {Array.from({ length: 10 }).map((_, i) => (
             <li key={i} />
           ))}
         </ul>
-      </div>      
+      </div>        
+
+      <div className="relative z-10 max-w-4xl text-center space-y-6 px-6">
+        <h1 className="text-white text-5xl leading-tight">
+          I am a final year computer science student at{" "}
+          <a
+            href="https://www.unsw.edu.au/"
+            target="_blank"
+            className="underline underline-offset-4 hover:no-underline"
+            rel="noreferrer"
+          >
+            UNSW
+          </a>
+        </h1>
+
+        <p className="text-2xl font-mono text-white">
+          <Typewriter
+            words={[
+              "I'm a frontend developer",
+              "I'm a fullstack developer",
+              "I'm a game developer",
+              "I'm a chatgpt enthusiast and CEO of buggy codes",
+              "I'm UNEMPLOYED :(",
+            ]}
+            loop={1}
+            cursor
+            cursorStyle="|"
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+          />
+        </p>
+
+        
+      </div>
     </div>
   ) 
 }
